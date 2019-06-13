73f2cb0c381a40d0981b0806ba825b5a99c8ac4c
小島祐太 <oha3641@icloud.com>
2019-06-13 14:58:29 +0900

    TypeScript_20190613

---
+++ TypeScript/index.js
@@ -0,0 +1,6 @@
+var PI = 3.14;
+PI = 123;
+var today = new Date();
+var _a = [today.getFullYear(), today.getMonth() + 1,
+    today.getDate()], year = _a[0], month = _a[1], day = _a[2];
+document.getElementById('date').innerHTML = '${year}/${month}/${day}';
